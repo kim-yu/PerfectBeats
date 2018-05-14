91a68c308d31c62a63d16ef468bebe5a854a41f7
kim-yu <kimberly.yu1@gmail.com>
2018-05-13 22:40:20 -0400

    made drums larger

--- app/config.js
+++ app/config.js
@@ -7,19 +7,19 @@ var Colors = {
   YELLOW: "#FAF36F", // highlighting
 };
 
-var BONGOSIZE1 = 250;
-var BONGOSIZE2 = 200;
-var BONGOX = -300;
+var BONGOSIZE1 = 300;
+var BONGOSIZE2 = 250;
+var BONGOX = -350;
 var BONGOY = -100;
 
-var CONGASIZE1 = 200;
-var CONGASIZE2 = 200;
-var CONGAX = 300;
+var CONGASIZE1 = 300;
+var CONGASIZE2 = 300;
+var CONGAX = 325;
 var CONGAY = -100;
 
-var DJEMBESIZE = 250;
+var DJEMBESIZE = 300;
 var DJEMBEX = 0;
-var DJEMBEY = 190;
+var DJEMBEY = 170;
 
 var CURSORSIZE = 20;
 var BASS_RATIO = 0.5;
